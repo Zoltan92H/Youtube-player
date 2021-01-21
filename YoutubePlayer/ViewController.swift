00bd7ea4eeeeebbafd8203454ce21b7602e1c397
Zoltán Hidegkuti <zoltanhidegkuti92@gmail.com>
2021-01-21 17:26:43 +0100

    initial xcode project creation

---
+++ YoutubePlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubePlayer
+//
+//  Created by Hidegkuti Zoltán on 2021. 01. 21..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
